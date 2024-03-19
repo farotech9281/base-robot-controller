5dc128ae04c7b82fe63f51fb92304ffa93ee3186
Sasser7 <sasser7.dev@gmail.com>
2024-03-19 08:47:55 +0100

    Big code update

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,29 +8,43 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <ctre/Phoenix.h>
 #include <frc/XboxController.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
 
 class Robot : public frc::TimedRobot
 {
   ctre::phoenix::motorcontrol::can::TalonSRX retractor{9}, r_retractor{8}, l_retractor{7};
+  ctre::phoenix::motorcontrol::can::VictorSPX m_frontLeft{1}, m_rearLeft{2}, m_frontRight{3}, m_rearRight{4};
+
+  frc::DifferentialDrive m_drive{[&](double output)
+                                 { m_frontLeft.Set(VictorSPXControlMode::PercentOutput, output); },
+                                 [&](double output)
+                                 { m_frontRight.Set(VictorSPXControlMode::PercentOutput, output); }};
   frc::XboxController drv_pad{0};
   bool enabled{false};
 
 public:
   Robot()
   {
-    r_retractor.SetInverted(true);
-    l_retractor.SetInverted(true);
   }
 
   void RobotInit() override
   {
+
+    r_retractor.SetInverted(true);
+    l_retractor.SetInverted(true);
+
+    m_rearLeft.Follow(m_frontLeft);
+    m_rearRight.Follow(m_frontRight);
   }
 
   void TeleopPeriodic() override
   {
+
+    m_drive.TankDrive(-drv_pad.GetLeftY(), -drv_pad.GetRightY());
+
     if (drv_pad.GetAButton())
     {
-      //TODO: Set 7 more power than 8
+      // TODO: Set 7 more power than 8
       retractor.Set(TalonSRXControlMode::PercentOutput, 0.75);
       r_retractor.Set(TalonSRXControlMode::PercentOutput, 1.2);
       l_retractor.Set(TalonSRXControlMode::PercentOutput, 1.2);
